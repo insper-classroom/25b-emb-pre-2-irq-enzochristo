ea82acb9cf9043ee6ae8dbd5fb3e8e52e1f034de
enzochristo <enzosc1@al.insper.edu.br>
2025-08-23 18:44:26 -0300

    ex3.0

--- exe3/main.c
+++ exe3/main.c
@@ -6,12 +6,17 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+int volatile green_flag = 0;
+int volatile red_flag = 0;
+
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {  // fall edge
         if (gpio == BTN_PIN_R) {
-            printf("fall red\n");
+            red_flag = 1;
+            
         } else if (gpio == BTN_PIN_G) {
-            printf("fall green\n");
+            green_flag = 1;
         }
     }
 }
@@ -35,5 +40,24 @@ int main() {
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
     while (true) {
+
+        if (red_flag)
+        {
+            printf("fall red\n");
+            red_flag = 0;
+
+
+        }
+        
+
+        if (green_flag)
+        {
+            printf("fall green\n");  
+            green_flag = 0; 
+        }
+        
+
+
+
     }
 }
