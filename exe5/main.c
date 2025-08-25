7ba9394a2ccf815cd3c58937fca05781b2b438ea
enzochristo <enzosc1@al.insper.edu.br>
2025-08-24 22:18:43 -0300

    ex5.0

--- exe5/main.c
+++ exe5/main.c
@@ -5,18 +5,21 @@
 
 
 const int BTN_PIN_R = 28;
-
-int volatile flag = 0;
+time_t sec;
+time_t stop;
+int volatile flag = 1;
+int cnt = 8;
 
 
 
 void btn_callback(uint gpio, uint32_t events){
     if (events == 0x04)
     {
-       
+        flag = 0;
+    }
 
-        // flag = start - 800 > 0.001;
-        // flag =1;
+    else if (events == 0x08){
+        flag = 1;
     }
 }
 
@@ -27,17 +30,34 @@ int main() {
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback); // Sempre na main!
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL , true, &btn_callback); // Sempre na main!
 
     while (true) {
+        
+        if (!flag){
+            for (int i=0; i <cnt; i++){
+                sleep_ms(100);
+                if(flag == 1){
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                printf("Aperto longo!\n");
+                flag = 2;
+            }
+            else if (flag ==1)
+            {
+                printf("Aperto curto!\n");
+                flag = 2;
+            }
+            
+        }
 
-        printf("%d", flag);
-        // if (flag)
-        // {
-        //     printf("Aperto curto!\n");
-        //     flag = 0;
-
+        // if(!flag){
+        //     flag = 2;
         // }
+        
       
         
     }
